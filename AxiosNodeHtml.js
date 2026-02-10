a22ccecde9fc0db33417072f4b5d3c7ada8eed12
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2026-02-10 15:08:16 +0700

    check01

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://localhost:5000";
+const base_url = "http://localhost:3000";
 //const base_url = "http://node65644-rach-app.proen.app.ruk-com.cloud";
 
 // Set the template engine
